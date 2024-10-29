bebb6c55142541f42b2a6e31d164917176460e95
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-10-29 11:48:10 +0100

    Create & Update actions

---
+++ routes/posts.js
@@ -0,0 +1,72 @@
+import express from 'express';
+
+const router = express.Router();
+
+let posts = [
+	{
+		id: 1,
+		title: 'Post One',
+	},
+	{
+		id: 2,
+		title: 'Post Two',
+	},
+	{
+		id: 3,
+		title: 'Post Three',
+	},
+];
+
+// Get all posts
+router.get('/', (req, res) => {
+	const limit = parseInt(req.query.limit);
+	if (!isNaN(limit) && limit > 0) {
+		return res.status(200).json(posts.slice(0, limit));
+	}
+	res.status(200).json(posts);
+});
+
+// Get single post
+router.get('/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const post = posts.find((post) => post.id === id);
+
+	if (!post) {
+		return res.status(404).json({ message: `A post with the id of ${id} was not found` });
+	}
+	res.status(200).json(post);
+});
+
+// Create single post
+router.post('/', (req, res) => {
+	const newPost = {
+		id: posts.length + 1,
+		title: req.body.title,
+	};
+
+	if (!newPost.title) {
+		return res.status(400).json({ message: 'Please include a title' });
+	}
+
+	posts.push(newPost);
+	res.status(201).json(newPost);
+});
+
+// Change current post
+router.put('/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	let post = posts.find((post) => post.id === id);
+
+	if (!post) {
+		return res.status(404).json({ message: `The post with id: ${id} does not exist` });
+	}
+
+	if (!req.body.title) {
+		return res.status(400).json({ message: 'Please include a title' });
+	}
+
+	post.title = req.body.title;
+	res.status(200).json(post);
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import posts from './routes/posts.js';
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,42 +17,8 @@ const __dirname = path.dirname(__filename);
 // Setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-	{
-		id: 1,
-		title: 'Post One',
-	},
-	{
-		id: 2,
-		title: 'Post Two',
-	},
-	{
-		id: 3,
-		title: 'Post Three',
-	},
-];
-
-// Get all posts
-app.get('/api/posts', (req, res) => {
-	const limit = parseInt(req.query.limit);
-	if (!isNaN(limit) && limit > 0) {
-		res.status(200).json(posts.slice(0, limit));
-	} else {
-		res.status(200).json(posts);
-	}
-});
-
-// Get single post
-app.get('/api/posts/:id', (req, res) => {
-	const id = parseInt(req.params.id);
-	const post = posts.find((post) => post.id === id);
-
-	if (!post) {
-		res.status(404).json({ message: `A post with the id of ${id} was not found` });
-	} else {
-		res.status(200).json(post);
-	}
-});
+// Routes
+app.use('/api/posts', posts);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
