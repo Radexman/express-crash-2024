b9b8dc371cdafb18a7b226d29dea7357c6c028e0
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-10-29 11:58:23 +0100

    Delete Request

--- routes/posts.js
+++ routes/posts.js
@@ -55,7 +55,7 @@ router.post('/', (req, res) => {
 // Change current post
 router.put('/:id', (req, res) => {
 	const id = parseInt(req.params.id);
-	let post = posts.find((post) => post.id === id);
+	const post = posts.find((post) => post.id === id);
 
 	if (!post) {
 		return res.status(404).json({ message: `The post with id: ${id} does not exist` });
@@ -67,6 +67,19 @@ router.put('/:id', (req, res) => {
 
 	post.title = req.body.title;
 	res.status(200).json(post);
+});
+
+// Delete post
+router.delete('/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const post = posts.find((post) => post.id === id);
+
+	if (!post) {
+		return res.status(404).json({ message: `The post with id: ${id} does not exist` });
+	}
+
+	posts = posts.filter((post) => post.id !== id);
+	res.status(200).json(posts);
 });
 
 export default router;
